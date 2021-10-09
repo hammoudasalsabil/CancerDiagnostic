9b38fe3c4e45c04cd4ab9bc18e8bdf37603312aa
Salsabil Hammouda <hammoudasalsabilgl@gmail.com>
2021-10-09 10:48:29 +0100

    ADD-Alert

--- FrontEnd/CancerDiagnostic/src/app/medecin/layout/navbar/navbar.component.ts
+++ FrontEnd/CancerDiagnostic/src/app/medecin/layout/navbar/navbar.component.ts
@@ -4,6 +4,8 @@ import { AuthService } from 'src/app/services/auth.service';
 import { User } from 'src/app/auth.model';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { SharedService } from 'src/app/services/shared.service';
+import { ITblAlerts, TblAlerts } from 'src/app/model/alert.model';
 
 @Component({
   selector: 'app-navbar',
@@ -13,12 +15,24 @@ import { Subscription } from 'rxjs';
 export class NavbarComponent implements OnInit, OnDestroy {
   isAuthenticated:boolean = false;
   private userSub: Subscription;
-  constructor(private authService: AuthService, private router: Router ) { }
+  alertlist:any= [];
+  nbrDiagBreastOne:number;
+  nbrDiagBreastTwo:number;
+  nbrDiagBrainOne:number;
+  nbrDiagBrainTow:number;
+
+  nbrDiagTotal:number;
+  thblAlert:ITblAlerts={};
+  nbrAlert:number;
+
+  constructor(private authService: AuthService, private router: Router,private Service: SharedService ) { }
 
   ngOnInit(): void {
     this.userSub = this.authService.user.subscribe((user)=>{
       this.isAuthenticated =!user? false: true;
     })
+    this.getAlert();
+    this.getNbrDiag();
   }
   ngOnDestroy(){
     this.userSub.unsubscribe();
@@ -27,5 +41,56 @@ export class NavbarComponent implements OnInit, OnDestroy {
     this.authService.logout();
     this.router.navigate([''])
   }
+  getAlert(){
+    this.Service.getAlert().subscribe(data=>{
+      this.alertlist=data;
+      this.nbrAlert=data.length;
+      console.log("alertlist lawla = ",this.alertlist)
+    })
+  }
+  getNbrDiag(){
+  this.nbrDiagBreastOne=0;
+  this.nbrDiagBreastTwo=0;
+  this.nbrDiagBrainOne=0;
+  this.nbrDiagBrainTow=0;
+  this.nbrDiagTotal=0;
+
+  this.Service.getDiagnosticBreastOne().subscribe(data=>{
+    this.nbrDiagBreastOne=data.length;
+    console.log("nbrDiagBreastOne = ",this.nbrDiagBreastOne)
+
+    this.Service.getDiagnosticBrainOne().subscribe(data=>{
+      this.nbrDiagBrainOne=data.length;
+      console.log("nbrDiagBrainOne = ",this.nbrDiagBrainOne);
+
+      this.Service.getDiagnosticBreastTwo().subscribe(data=>{
+        this.nbrDiagBreastTwo=data.length;
+        console.log("nbrDiagBreastTwo = ",this.nbrDiagBreastTwo)
+
+        this.Service.getDiagnosticBrainTow().subscribe(data=>{
+          this.nbrDiagBrainTow=data.length;
+          console.log("nbrDiagBrainTow = ",this.nbrDiagBrainTow)
+          //total:
+          this.nbrDiagTotal=this.nbrDiagBreastOne + this.nbrDiagBrainOne + this.nbrDiagBreastTwo +this.nbrDiagBrainTow;
+          console.log("nbrDiagTotal = ",this.nbrDiagTotal);
+         
+          if(this.nbrDiagTotal > 15){
+
+            this.thblAlert.Text=  "abdgffggdfdsszeqdgrts";
+            this.thblAlert.Date="2021-08-11";
+            this.Service.addAlert(this.thblAlert).subscribe(res=>{
+              console.log("alerts==",this.thblAlert)
+            });
+  
+          }
+        })
+    
+      
+      })
+    })
+  })
+  }
+
+  
 
 }
