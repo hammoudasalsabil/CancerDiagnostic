c3a70621bab4824c54e8052052ff509707a94665
Salsabil Hammouda <hammoudasalsabilgl@gmail.com>
2021-10-24 21:22:18 +0100

    Add-Resultat1

--- FrontEnd/CancerDiagnostic/src/app/medecin/layout/container/resultatdiagnosticseinone/resultatdiagnosticseinone.component.ts
+++ FrontEnd/CancerDiagnostic/src/app/medecin/layout/container/resultatdiagnosticseinone/resultatdiagnosticseinone.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-resultatdiagnosticseinone',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resultatdiagnosticseinone.component.scss']
 })
 export class ResultatdiagnosticseinoneComponent implements OnInit {
-
+  addResForm: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
